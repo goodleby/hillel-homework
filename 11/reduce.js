13953abb0255011f4b114af11ae72a16555121a6
Alex Chebotarsky <goodleby@gmail.com>
2019-12-07 01:09:13 +0200

    Fixed issue with reduce that it was taking wrong start value. Now its checking for emptyness and making right start value

--- 11/reduce.js
+++ 11/reduce.js
@@ -1,10 +1,17 @@
-Array.prototype.accumulate = function(cb, startValue = 0) {
+Array.prototype.accumulate = function(cb, startValue) {
+    if(this.length === 0 && startValue !== undefined) return startValue;
+    if(this.length !== 0 && startValue === undefined) {
+        startValue = this[0];
+        var iMod = 1;
+    }
     var acc = startValue;
-    for(var i = 0; i < this.length; i++) {
+    for(var i = iMod ? iMod : 0; i < this.length; i++) {
         acc = cb(acc, this[i], i, this);
     }
     return acc;
 }
+
+
 
 var grades = [12, 10, 11, 9, 7, 10, 11];
 var avg = grades.accumulate((sum, item) => sum += item) / grades.length;
